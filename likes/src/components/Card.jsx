ce7115e158384800434ba95e0d3303cc8a3c8f70
WangaiJM <jahkeyjohn@gmail.com>
2024-03-13 23:41:17 +0300

    like functionality

--- likes/src/components/Card.jsx
+++ likes/src/components/Card.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { CiFaceSmile } from "react-icons/ci";
 import { TbBrandWechat } from "react-icons/tb";
 import { CiHeart } from "react-icons/ci";
@@ -7,10 +9,23 @@ import Image from "./../static/images/dog.jpg";
 import "./card.css";
 
 const Card = () => {
+  const [like, setLike] = useState(false);
+  const [count, SetCount] = useState(0);
+
+  const toggleLike = () => {
+    if (!like) {
+      setLike(true);
+      SetCount(count + 1);
+    } else {
+      setLike(false);
+      SetCount(count - 1);
+    }
+  };
+
   return (
     <div className="main">
       <h1 className="title">Double Click to like</h1>
-      <h2 className="subtitle">Likes: 0</h2>
+      <h2 className="subtitle">{`Likes: ${count}`}</h2>
       <div className="card">
         <div className="card-header">
           <span>
@@ -19,11 +34,15 @@ const Card = () => {
           <h1>Doggy</h1>
         </div>
         <div className="card-body">
-          <img src={Image} alt="dog" />
+          <img src={Image} alt="dog" onDoubleClick={toggleLike} />
         </div>
         <div className="card-footer">
           <TbBrandWechat />
-          <CiHeart className="active" />
+          {like ? (
+            <CiHeart className="active" onClick={toggleLike} />
+          ) : (
+            <CiHeart onClick={toggleLike} />
+          )}
         </div>
       </div>
     </div>
